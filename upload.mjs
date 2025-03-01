f07e4af197403a5948d41047fc3801947b0124b0
hag <hag@hag>
2025-03-01 23:16:30 +0100

    ignore some errors

--- upload.mjs
+++ upload.mjs
@@ -4,12 +4,21 @@ import { request } from "node:http"
 const main = () => {
 	const paths = process.env["OUT_PATHS"]
 	return new Promise((resolve, reject) => {
-		const req = request("http://localhost:18008/upload", { method: "POST" }, (res) => {
-			res.on("end", resolve)
-			res.on("error", reject)
-		})
-		req.write(paths)
-		req.end()
+		try {
+			const req = request("http://localhost:18008/upload", { method: "POST" }, (res) => {
+				res.on("end", resolve)
+				res.on("error", reject)
+			})
+			req.on("error", reject)
+			req.write(paths)
+			req.end()
+		} catch (e) {
+			reject(e)
+		}
 	})
 }
-await main()
+try {
+	await main()
+} catch (e) {
+	console.debug(e)
+}
